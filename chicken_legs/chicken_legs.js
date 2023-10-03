87d5a9e08d5165e75e0de1b7d03f4534f2a8e332
naomiuwa <naomi.uwamungu@gmail.com>
2023-10-03 09:15:53 +0100

    started writing a function that accepts a number of chickens and returns the number of the legs they have

---
+++ chicken_legs/chicken_legs.js
@@ -0,0 +1,13 @@
+/* Write a function that accepts a number of chickens and returns the number of legs the chicken have. 
+
+For example: if someone enters 4 chickens each chicken has 2 legs so you return 8 */
+
+let chickens = 2;
+
+function countTheChickensLegs(chickens) {
+  console.log(chickens);
+}
+
+//PLAN
+//multiply the number of chickens by 2
+//return the number of legs the chickens have
